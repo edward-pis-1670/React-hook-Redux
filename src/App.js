01bce2422d6b25d6fef49eb227815ac283c7a9f4
edward-pis-1670 <71159568+edward-pis-1670@users.noreply.github.com>
2021-07-09 16:16:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+import Person from './Person/Person.js'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [personState, setPersonState] = useState({
+    persons: [
+      {name:'Max', age:28},
+      {name:'Manu', age:26},
+      {name:'Stephanie', age:27},
+
+    ],
+    otherState: 'some other value'
+})
+
+const switchNameHandler = (newName) => {
+  setPersonState({
+    persons: [
+      {name:newName, age:28},
+      {name:'Nam', age:26},
+
+    ]
+  })
+}
+const nameHandler = (e) => {
+  setPersonState({
+    persons: [
+      {name:'Hoa', age:28},
+      {name:e.target.value, age:26},
+
+    ]
+  })
+}
+  return <div className="App">
+    <h1>Hi, i'm React</h1>
+    <button onClick={() => switchNameHandler('Ngang')}>Switch Name</button>
+    <Person click={() => switchNameHandler} name={personState.persons[0].name}/>
+    <Person changed={nameHandler} click={switchNameHandler} name={personState.persons[1].name}/>
+  </div>;
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import './Person.css'
+const Person = (props) => {
+    return ( 
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
+            <input type="text" onChange={props.changed}/>
+        </div>
+     );
+}
+ 
+export default Person;
