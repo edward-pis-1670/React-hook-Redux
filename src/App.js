7cd3bf71e10ba9f0fa8b8329d23e4197112655dd
edward-pis-1670 <71159568+edward-pis-1670@users.noreply.github.com>
2021-07-09 16:57:13 +0700

    update1

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
 import "./App.css";
 import React, { useState } from "react";
-import Person from './Person/Person.js'
+import Person from "./Person/Person.js";
 function App() {
-
   const [personState, setPersonState] = useState({
     persons: [
-      {name:'Max', age:28},
-      {name:'Manu', age:26},
-      {name:'Stephanie', age:27},
-
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 26 },
+      { name: "Stephanie", age: 27 },
     ],
-    otherState: 'some other value'
-})
+    otherState: "some other value",
+    showPersons: false,
+  });
 
-const switchNameHandler = (newName) => {
-  setPersonState({
-    persons: [
-      {name:newName, age:28},
-      {name:'Nam', age:26},
+  const switchNameHandler = (newName) => {
+    setPersonState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Nam", age: 26 },
+      ],
+    });
+  };
+  const nameHandler = (e) => {
+    setPersonState({
+      persons: [
+        { name: "Hoa", age: 28 },
+        { name: e.target.value, age: 26 },
+      ],
+    });
+  };
 
-    ]
-  })
-}
-const nameHandler = (e) => {
-  setPersonState({
-    persons: [
-      {name:'Hoa', age:28},
-      {name:e.target.value, age:26},
-
-    ]
-  })
-}
-  return <div className="App">
-    <h1>Hi, i'm React</h1>
-    <button onClick={() => switchNameHandler('Ngang')}>Switch Name</button>
-    <Person click={() => switchNameHandler} name={personState.persons[0].name}/>
-    <Person changed={nameHandler} click={switchNameHandler} name={personState.persons[1].name}/>
-  </div>;
+  const togglePersonHandler = () => {
+    const doesShow = personState.showPersons;
+    setPersonState({ showPersons: !doesShow });
+  };
+  const style = {
+    backgroundColor: "white",
+    font: "inherit",
+    border: "1px solid blue",
+    padding: "8px",
+    cursor: "pointer",
+  };
+  return (
+    <div className="App">
+      <h1>Hi, i'm React</h1>
+      <button style={style} onClick={togglePersonHandler}>
+        Switch Name
+      </button>
+      {personState.showPersons ? (
+        <div>
+          <Person
+            click={() => switchNameHandler}
+            name={personState.persons[1].name}
+          />
+          <Person
+            changed={nameHandler}
+            click={switchNameHandler}
+            name={personState.persons[1].name}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default App;
