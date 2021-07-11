989b2140b359ec59ba81bf42208171bb1705a100
Edward1670 <Edward.pis.1670@gmail.com>
2021-07-12 06:07:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,66 +1,82 @@
 import "./App.css";
 import React, { useState } from "react";
+import Radium from 'radium';
 import Person from "./Person/Person.js";
 function App() {
   const [personState, setPersonState] = useState({
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 26 },
-      { name: "Stephanie", age: 27 },
+      { id: 1, name: "Max", age: 28 },
+      { id: 2, name: "Manu", age: 26 },
     ],
     otherState: "some other value",
-    showPersons: false,
   });
+  const [showForm, setShowForm] = useState({ showPersons: false });
 
-  const switchNameHandler = (newName) => {
+  const deletePersonHandler = (personIndex) => {
+    const persons = [...personState.persons];
+    persons.splice(personIndex, 1);
+    setPersonState({ persons: persons });
+  };
+  const changeNameHandler = (e, id) => {
+    const personIndex = personState.persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = { ...personState.persons[personIndex] };
+    person.name = e.target.value;
+    const persons = [...personState.persons];
+    persons[personIndex] = person;
     setPersonState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Nam", age: 26 },
-      ],
+      persons: persons,
     });
   };
-  const nameHandler = (e) => {
-    setPersonState({
-      persons: [
-        { name: "Hoa", age: 28 },
-        { name: e.target.value, age: 26 },
-      ],
-    });
-  };
-
   const togglePersonHandler = () => {
-    const doesShow = personState.showPersons;
-    setPersonState({ showPersons: !doesShow });
+    const doesShow = showForm.showPersons;
+    setShowForm({ showPersons: !doesShow });
   };
   const style = {
-    backgroundColor: "white",
+    backgroundColor: "green",
+    color: "white",
     font: "inherit",
     border: "1px solid blue",
     padding: "8px",
     cursor: "pointer",
   };
+  let persons = null;
+  if (showForm.showPersons) {
+    persons = (
+      <div>
+        {personState.persons.map((person, index) => {
+          return (
+            <Person
+              key={person.id}
+              click={() => deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              changed={(e) => changeNameHandler(e, person.id)}
+            />
+          );
+        })}
+      </div>
+    );
+    style.backgroundColor = "red";
+  }
+  let classes = [];
+  if(personState.persons.length <= 2) {
+    classes.push('red');
+  }
+  if(personState.persons.length <= 1) {
+    classes.push('bold');
+  }
   return (
     <div className="App">
       <h1>Hi, i'm React</h1>
+      <p className={classes.join(' ')}>This is my course</p>
       <button style={style} onClick={togglePersonHandler}>
-        Switch Name
+        Toggle person
       </button>
-      {personState.showPersons ? (
-        <div>
-          <Person
-            click={() => switchNameHandler}
-            name={personState.persons[1].name}
-          />
-          <Person
-            changed={nameHandler}
-            click={switchNameHandler}
-            name={personState.persons[1].name}
-          />
-        </div>
-      ) : null}
+      {persons}
     </div>
   );
 }
 
-export default App;
+export default Radium(App);
